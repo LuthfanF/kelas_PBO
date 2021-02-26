3d30953b414eacd664375808d87bbbf128fc3fcb
LuthfanF <mluthfanfaohan1@gmail.com>
2021-02-26 09:17:24 +0700

    aktifitas 1

---
+++ aktifitas1.py
@@ -0,0 +1,11 @@
+#INPUT
+
+nama=input ("Mohammad Luthfan Faohan")
+umur=19
+
+#OUTPUT#
+print("")
+print("====INPUT DAN OUTPUT PADA BAHASA PEMROGAMAN PYTHON====")
+print("Mohammad Luthfan Faohan", nama)
+print("19 tahun", umur)
+print("")
