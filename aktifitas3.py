cfbd02fcaf1fd4bb78ce9a87f00bbb3845b152a6
LuthfanF <mluthfanfaohan1@gmail.com>
2021-02-26 09:52:37 +0700

    aktifitas 3

---
+++ aktifitas3.py
@@ -0,0 +1,14 @@
+#PERCABANGAN
+print("percobaan pada bahasa pemrogaman python")
+print("pilih bahasa pemrogaman: ")
+print("1. python")
+print("2. C")
+
+pilih=int(input("pilih menu (1/2) : "))
+
+if pilih == 1:
+    print("bahasa pemrogaman yang dipilih adalah python")
+elif pilih  == 2:
+    print("bahasa pemrogaman yang dipilih adalah C")
+else :
+    print("pilihan Anda tidak tersedia!!:")
